0c4a7774cc3048070a9bd9c9d800765aa5971080
Chip Boling <chip@bcsw.net>
2018-11-10 16:32:22 -0600

    Finished refactoring of ME definition vs ME instance & packet operations

--- golang/basetemplates.py
+++ golang/basetemplates.py
@@ -18,7 +18,7 @@ import os
 from . import COPYRIGHT, GENERATOR_WARNING, PACKAGE_NAME, camelcase
 
 ME_FILENAME = '{}.go'
-ME_TEMPLATES = ['attribute', 'omcidefs', 'messagetypes', 'msgbase']
+ME_TEMPLATES = ['attribute', 'omcidefs', 'mebase']
 
 # Set up filters for this module
 jinja2.filters.FILTERS['camelcase'] = camelcase
